1692297f364be33e7517842b6a9ac0e2f9a8d21d
kal-aditya-al <aditya777kr@gmail.com>
2022-10-22 13:19:47 +0530

    using replace() method

---
+++ Goal Parser Interpretation.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String interpret(String command) {
+        
+        String str = command
+                .replace("()","o")
+                .replace("(al)","al");
+              
+            return str;
+            
+    }
+}
