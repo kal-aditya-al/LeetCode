e506dcde704932a98d9d7d979777a0927141d9f6
kal-aditya-al <aditya777kr@gmail.com>
2022-12-24 14:24:04 +0530

    using indexOf

---
+++ index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,6 @@
+ public class Solution {
+public int strStr(String haystack, String needle) {
+    if(needle.equals("") || needle.length()==0) return 0;
+        return haystack.indexOf(needle);
+}
+ }
